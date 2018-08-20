67ab81ac7ea07331b8bad078bd672973a382051c
Constantine Mars <constantine.mars@gmail.com>
2018-08-21 02:33:28 +0300

    Product component with properties

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+enum Category {
+  Electronics,
+  Kitchen,
+  Books
+}
+
+type Authors = Array<{id: number, text: string}>;
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -7,9 +15,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  name = 'The Angular 6 Book';
+  description = 'The best seller of year 2018';
+  price = 50;
+  category = Category.Books;
+  isAvailable = true;
+  authors: Authors = [
+    {id: 1, text: 'Maximilian Schwarzmüller'},
+    {id: 2, text: 'Mosh Hamedani'},
+    {id: 3, text: 'Anthony Alicea'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
